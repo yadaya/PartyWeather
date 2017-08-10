84359d094d676b62af9f68b40e5608bd5f80aa98
Mauricio Acuña <Mauricio_Acuna@intranet.itresources>
2017-08-10 16:36:34 -0300

    Cambios de ButterKnife

---
+++ app/src/main/java/com/example/macuna/colorweather/HourlyWeatherctivity.java
@@ -0,0 +1,13 @@
+package com.example.macuna.colorweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HourlyWeatherctivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_weatherctivity);
+    }
+}
--- app/src/main/java/com/example/macuna/colorweather/MainActivity.java
+++ app/src/main/java/com/example/macuna/colorweather/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.macuna.colorweather;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+   // private TextView dailyWeatherTextView;
+   // private TextView hourlyWeatherTextView;
+   // private TextView minutelyWeatherTextView;
+
+    //@BindView(R.id.dayliWeatherTextView) TextView dailyWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
+
+
+    @OnClick(R.id.dayliWeatherTextView)
+    public void dailyWeatherClick(){
+
+        Intent dailyActivityIntent = new Intent(MainActivity.this, DayliWeatherActivity.class);
+        startActivity(dailyActivityIntent);
+    }
+
+    @OnClick(R.id.hourlyWeatherTextView)
+    public void hourlyWeatherClick(){
+        Intent hourlyActivityIntent = new Intent(MainActivity.this, HourlyWeatherctivity.class);
+        startActivity(hourlyActivityIntent);
+    }
+
+    @OnClick(R.id.minutelyWeatherTextView)
+    public void minutelyWeatherClick(){
+        Intent minutelyActivityIntent = new Intent(MainActivity.this, MinuteliWeatherActivity.class);
+        startActivity(minutelyActivityIntent);
+    }
+
 }
